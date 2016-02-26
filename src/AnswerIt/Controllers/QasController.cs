2d0b5d6d5f8cd9c0163ac8effd654d8ce935ef71
fiyazbinhasan <fiyazhasan@live.com>
2016-02-27 01:43:51 +0600

    Answer It First Commit

---
+++ src/AnswerIt/Controllers/QasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.AspNet.Hosting;
+using AnswerIt.Models;
+
+namespace AnswerIt.Controllers
+{
+    [Route("api/[controller]")]
+    public class QasController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Question> Get()
+        {
+            var virtualPath = "data/qas.json";
+            var env = new HostingEnvironment();
+
+            if (string.IsNullOrWhiteSpace(env.WebRootPath))
+            {
+                env.WebRootPath = Directory.GetCurrentDirectory();
+            }
+
+            var filePath = env.MapPath(virtualPath);
+
+            var json = System.IO.File.ReadAllText(filePath);
+            var qas = JsonConvert.DeserializeObject<IEnumerable<Question>>(json);
+
+            return qas;
+        }
+    }
+}
---
+++ src/AnswerIt/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace AnswerIt.Models
+{
+    public class Question
+    {
+        public Question()
+        {
+            Answers = new List<Answer>();
+        }
+        [JsonProperty("question")]
+        public string Text { get; set; }
+        [JsonProperty("answers")]
+        public ICollection<Answer> Answers { get; set; }
+    }
+}
